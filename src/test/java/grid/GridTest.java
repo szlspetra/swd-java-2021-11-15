a75d9b35418a43fb0c38e3c2abedf35a266e7f14
szlspetra <szls.petra@gmail.com>
2021-11-17 09:28:20 +0100

    wizard html

--- src/test/java/grid/GridTest.java
+++ src/test/java/grid/GridTest.java
@@ -10,12 +10,21 @@ import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SeleniumTest
 class GridTest {
+	
+	@Test
 	void testGrid(WebDriver driver, URL url) {
-		 driver.get(url + "/welcome");
+		 driver.get(url + "/grid/index.html");
+		 var td = driver.findElements(By.tagName("tr")).get(1).findElements(By.tagName("td"));
+		 
+		 assertThat(td)
+	         .extracting(WebElement::getText)
+	         .containsExactlyInAnyOrder("4","5","6");
+	
 	}
 }
